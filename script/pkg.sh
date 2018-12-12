4056d6fc24ca44e45d88b997340c67fbeed28d99
legendecas <legendecas@gmail.com>
2018-12-12 17:40:50 +0800

    chore: enable yoda-pm packaging

--- script/pkg.sh
+++ script/pkg.sh
@@ -4,5 +4,6 @@ export PKG_CACHE_PATH=$HOME/.pkg
 
 cd yoda-cli
   npx pkg --out-path ../build ./bin/yoda-am
+  npx pkg --out-path ../build ./bin/yoda-pm
   npx pkg --out-path ../build ./bin/yoda-debug
 cd -
