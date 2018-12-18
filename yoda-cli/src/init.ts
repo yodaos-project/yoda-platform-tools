e00a194741da938c7c88f7d4ee1b0cb68da2c63d
legendecas <legendecas@gmail.com>
2018-12-18 17:37:25 +0800

    cli: init prompts

--- yoda-cli/src/init.ts
+++ yoda-cli/src/init.ts
@@ -3,6 +3,7 @@ import * as path from 'path'
 import { promisify } from 'util'
 import * as glob from 'glob'
 import * as mustache from 'mustache'
+import * as prompts from 'prompts'
 import program from './program'
 import { mkdirp } from './util'
 
@@ -41,6 +42,70 @@ program
       throw new Error(`Target ${workDir} exists yet is not a directory.`)
     }
 
+    const questions: prompts.PromptObject[] = [
+      {
+        type: 'text',
+        name: 'name',
+        message: 'Name of the package'
+      },
+      {
+        type: 'text',
+        name: 'description',
+        message: 'A short description of the package'
+      },
+      {
+        type: 'toggle',
+        name: 'private',
+        message: 'Is this package private?',
+        initial: true,
+        active: 'yes',
+        inactive: 'no'
+      },
+      {
+        type: prev => !prev ? 'text' : undefined!,
+        name: 'license',
+        message: 'Publish license of the app'
+      },
+      {
+        type: 'list',
+        name: 'skills',
+        message: 'Skill ids of your app',
+        format (value: string[]) {
+          return value.filter(it => !!it)
+        }
+      },
+      {
+        type: 'list',
+        name: 'permissions',
+        message: 'Requested permissions of your app',
+        initial: 'ACCESS_TTS',
+        format (value: string[]) {
+          return value.filter(it => !!it)
+        }
+      },
+      {
+        type: 'list',
+        name: 'keywords',
+        message: 'Keywords',
+        format (value: string[]) {
+          return value.filter(it => !!it)
+        }
+      }
+    ]
+    const answers = await prompts(questions, { onCancel: () => process.exit(1) })
+    answers.skills = answers.skills.map((skillId: string) => ({ skillId }))
+    answers.permissions = answers.permissions.map((permission: string) => ({ permission }))
+    answers.keywords = answers.keywords.map((keyword: string) => ({ keyword }))
+    Object.keys(answers).forEach(it => {
+      if (!Array.isArray(answers[it])) {
+        return
+      }
+      if (answers[it].length === 0) {
+        return
+      }
+      answers[it][answers[it].length - 1].isLast = true
+    })
+
     const files = await globAsync(path.join(templatePath, '**', '*'))
     await Promise.all(files.map(async (file) => {
       const relative = path.relative(templatePath, file)
@@ -54,7 +119,7 @@ program
         await mkdirpAsync(dirname)
       }
       const content = await readFileAsync(file, 'utf8')
-      const output = mustache.render(content, { name: 'foobar' })
+      const output = mustache.render(content, answers)
       await writeFileAsync(target, output, 'utf8')
     }))
   })
