ce286b4c0d7c0f6a9ccb3add635f6118f8903537
legendecas <legendecas@gmail.com>
2019-03-30 16:36:05 +0800

    build: ci

--- script/pkg.sh
+++ script/pkg.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
-set -x
+set -ex
 
 export PKG_CACHE_PATH=$HOME/.pkg
 
+target=$(uname)
+
+if test $target = "Darwin"; then
+  target=macos
+else
+  target=linux
+fi
+
 cd yoda-cli
-  npx pkg -c package.json --out-path ../build ./bin/yoda
+  npx pkg -t node10-$target -c package.json --out-path ../build ./bin/yoda
+cd -
+
+cd yoda-platform-lib
+  cp $(node -e "console.log(path.join(path.dirname(require.resolve('flora')), 'flora-cli.node'))") ../build
 cd -
